bc4c7a0410635f3b3e66372e766b274c9b7cd2cc
Kaique Costa <kaiquenagato94@outlook.com>
2023-08-28 14:50:56 -0300

    add db and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_145745) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_173225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.string "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_orders_on_game_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_145745) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
+  add_foreign_key "orders", "games"
+  add_foreign_key "orders", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Game.destroy_all
+
+games = [
+  { name: "The Witcher 3: Wild Hunt", description: "Um RPG épico de mundo aberto.", price: 29.99, category: "RPG", user_id: 1 },
+  { name: "Valorant", description: "Um jogo de tiro tático em equipe.", price: 0, category: "FPS",  user_id: 1 }
+]
+
+games.each do |game|
+  Game.create!(game)
+end
