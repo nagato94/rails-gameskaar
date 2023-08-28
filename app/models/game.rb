a2067015b3b6f89be3a72e960138e966db7cd21e
Kaique Costa <kaiquenagato94@outlook.com>
2023-08-28 15:45:38 -0300

    add carroussel

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
+  has_many :orders
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
