5a68035b40507351bf49207dea1136d8efb33258
Alyson Pereira <alysonrichard2001@gmail.com>
2023-08-28 18:44:41 -0300

    new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,21 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:description, :price, :category)
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,2 @@
 class OrdersController < ApplicationController
-  def index
-    @orders = Order.all
-  end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = Orders.new(order_params)
-    if @order.save
-      redirect_to order_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def show
-    @order = Order.find(params[:id])
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:comment, :price, :game_id)
-  end
 end
