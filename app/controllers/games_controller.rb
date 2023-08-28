83f44144d42f65d3e973b18a343374ece3c778ab
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-08-28 18:35:07 -0400

    added games->show,index,new,create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,37 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @games = Game.all
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    # We need to create a new game using the current user
+    @game = Game.new(game_params)
+    @game.user = @user
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-    @order = Order.find(params[:id])
+    @game = Game.find(params[:id])
   end
 
   private
 
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
+    @user = current_user
+  end
+
   def game_params
-    params.require(:game).permit(:description, :price, :category)
+    params.require(:game).permit(:name, :description, :price, :category, :user_id)
   end
 end
