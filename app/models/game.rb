32dfb2aad58109a52b01bb6a24b54e5813c9a156
Alyson Pereira <alysonrichard2001@gmail.com>
2023-08-30 14:25:39 -0300

    add list category

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,11 @@ class Game < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  CATEGORY = %w[FPS Moba RPG MMO MMORPG Ação Aventura Puzzle Esportes Estratégia Simulação Terror].freeze
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :user_id, presence: true
   validates :image, presence: true
 end
